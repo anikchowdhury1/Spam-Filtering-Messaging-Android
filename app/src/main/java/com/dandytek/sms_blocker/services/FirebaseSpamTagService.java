442720fa4d9febe04c643bd73e3215fc71f0c189
Anik Chowdhury <anikchy.cuet93@gmail.com>
2019-09-29 15:19:03 +0600

    initiate firebase service and broadcast listener

---
+++ app/src/main/java/com/dandytek/sms_blocker/services/FirebaseSpamTagService.java
@@ -0,0 +1,16 @@
+package com.dandytek.sms_blocker.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class FirebaseSpamTagService extends Service {
+    public FirebaseSpamTagService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
