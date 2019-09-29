17113f659241c5277e88979e0fec66753a23375c
Anik Chowdhury <anikchy.cuet93@gmail.com>
2019-09-29 13:39:33 +0600

    modification in package name and firebase integration

---
+++ app/src/main/java/com/dandytek/sms_blocker/receivers/MMSBroadcastReceiver.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 Anton Kaliturin <dandytek@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.dandytek.sms_blocker.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.dandytek.sms_blocker.utils.DefaultSMSAppHelper;
+
+/**
+ * MMSBroadcastReceiver stub
+ */
+
+public class MMSBroadcastReceiver extends BroadcastReceiver {
+    private static final String MMS_RECEIVED = "android.provider.Telephony.WAP_PUSH_RECEIVED";
+    private static final String MMS_DELIVER = "android.provider.Telephony.WAP_PUSH_DELIVER";
+    private static final String MMS_TYPE = "application/vnd.wap.mms-message";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // check action
+        String action = intent.getAction();
+        String type = intent.getType();
+        if (action == null || type == null ||
+                !action.equals(getAction()) ||
+                !type.equals(MMS_TYPE)) {
+            return;
+        }
+
+        // if not default sms app
+        if (!DefaultSMSAppHelper.isDefault(context)) {
+            return;
+        }
+
+        // isn't implemented yet...
+    }
+
+    public static String getAction() {
+        return (DefaultSMSAppHelper.isAvailable() ? MMS_DELIVER : MMS_RECEIVED);
+    }
+}
---
+++ app/src/main/java/com/dandytek/sms_blocker/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.dandytek.sms_blocker.utils;
+
+public final class Constants {
+    public static final int PIE_API_VERSION = 28;
+}
