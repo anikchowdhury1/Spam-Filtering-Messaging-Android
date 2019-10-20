47499b27eb54d25810f314d498d4eb097daae26f
Anik Chowdhury <anikchy.cuet93@gmail.com>
2019-10-20 17:34:58 +0600

    update some filtering tags

--- app/src/main/java/com/dandytek/sms_blocker/receivers/SMSBroadcastReceiver.java
+++ app/src/main/java/com/dandytek/sms_blocker/receivers/SMSBroadcastReceiver.java
@@ -136,7 +136,8 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
                 "job", "alert", "deals", "deal", "best", "days", "common", "free", "Super",
                 "buy", "your", "day", "cashback", "bundle", "Loan", "data", "Govt", "Eid",
                 "Puja", "christmas", "+8801673889597","bl", "bd", "tax", "last",
-                "dhamaka", "dhonnobad", "app", "game", "ghurbo"};
+                "dhamaka", "dhonnobad", "app", "game", "ghurbo", "16368", "22200",
+                "carnival", "16480", "notun", "bondhu", "16449", "mobile", "tv", "16462", "16280"};
 
   /*      ArrayList<String> tags = new ArrayList<String>();
 
